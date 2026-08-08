d9ff6ed2aa6140623bd9993e6f3a048b30b31868
Shubham07jee <sc2631697@gmail.com>
2026-08-08 13:44:29 +0530

    Added navbar in base file and task container in index file.

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from flask import Flask   # first flask is library and second is class
+from flask import Flask , render_template  # first flask is library and second is class
 
+app = Flask(__name__)
 
+todo = [
+    {"Sr no" : 1, "title":"Sample Task",
+    "Desc":"This is a sample task for to do list","Status":"Pending"}
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html")  #After this writing we have import the render_template at top
+
+if __name__ == "__main__":
+    app.run(debug=True)

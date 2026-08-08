de14bf4233dac0a64bf99e6ee1f111f1ee2ef3bc
Shubham07jee <sc2631697@gmail.com>
2026-08-08 15:03:48 +0530

    Added table, buttons and alert section .

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 todo = [
     {"Sr no" : 1, "title":"Sample Task",
-    "Desc":"This is a sample task for to do list","Status":"Pending"}
+    "desc":"This is a sample task for to do list","date_created":"08-08-2026","Status":"Pending", "Action":""}
 ]
 
 @app.route("/")
 def home():
-    return render_template("index.html")  #After this writing we have import the render_template at top
+    return render_template("index.html",allTodos = todo)  #After this writing we have import the render_template at top
 
 if __name__ == "__main__":
     app.run(debug=True)
